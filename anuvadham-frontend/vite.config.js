1b77e84c36da4e60d6f6fcce397c166eb63b3859
kusujanakiram <bala.kusu@sasi.ac.in>
2025-06-01 18:44:04 +0530

    ui fix

--- anuvadham-frontend/vite.config.js
+++ anuvadham-frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 5174, // Now Vite will use port 5174
