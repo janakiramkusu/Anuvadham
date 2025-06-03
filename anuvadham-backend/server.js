2afe113f9811380df7da3e4aeaa7f29f9b9a586c
kusujanakiram <bala.kusu@sasi.ac.in>
2025-06-03 10:46:19 +0530

    UI fix

--- anuvadham-backend/server.js
+++ anuvadham-backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(cors({
-  origin: 'https://anuvadham-liart.vercel.app',
+  origin: 'https://anuvadham-three.vercel.app',
   credentials: true
 }));
 
